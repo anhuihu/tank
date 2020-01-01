4ccfc886a5aaa6d11ca83a61e341d8a6fd67c997
anhuihu <409701062@qq.com>
2020-01-01 23:34:17 +0800

    第二次修改添加TankFrame类，并重写paint方法

---
+++ src/com/tank/Mian.java
@@ -0,0 +1,25 @@
+package com.tank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Mian {
+    public static void main(String[] args) {
+        Frame f = new Frame();
+        f.setSize(800, 600);
+        f.setResizable(false);
+        f.setTitle("tank war");
+
+        f.setVisible(true);
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(100);
+            }
+        });
+
+
+    }
+
+}
